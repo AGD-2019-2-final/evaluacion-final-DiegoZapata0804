15c2918f5bff0f42a5ceb8d6d595f26d6da85614
Diego Alejandro Zapata Alcaraz <55606065+DiegoZapata0804@users.noreply.github.com>
2020-02-10 22:34:17 -0500

    Update mapper.py

--- 01-hadoop-50/q01-10/mapper.py
+++ 01-hadoop-50/q01-10/mapper.py
@@ -2,3 +2,13 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    #Sobre cada línea del (en total 1000)
+    for line in sys.stdin:
+        #Para cada una vamos a sacar la columna 2 ->credit_history
+        line = line.strip()
+        splits = line.split(",")
+        word = splits[2]
+        #Creamos el flujo de salida y a cada clave le damos valor 1
+        sys.stdout.write("{}\t1\n".format(word))
+        
